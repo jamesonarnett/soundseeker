06cd4d88d7d22665d24ba171354002be32130978
jamesonarnett <jamesonarnett@yahoo.com>
2022-09-27 17:37:25 -0500

    Adds background image, placeholder wip

--- components/MainCard.tsx
+++ components/MainCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginBtn from "./LoginBtn";
 import styles from "../styles/MainCard.module.scss";
 
@@ -6,6 +6,16 @@ const MainCard = () => {
   return (
     <section className={styles.cardBody}>
       <h1>soundSeeker</h1>
+
+      <div>
+        <h3>placeHolder</h3>
+        <input
+          type={"text"}
+          placeholder={"Enter your Spotify username"}
+          value={""}
+          onChange={() => {}}
+        />
+      </div>
       <LoginBtn />
     </section>
   );
